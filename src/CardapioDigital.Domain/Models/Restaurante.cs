18483bac6f9674d910583e28c9dc87618ea7e5d9
Wesley Farias <wesleyfarias.dev@gmail.com>
2024-12-01 03:30:39 -0300

    adiciondo propriedade NomeRestaurante

--- src/CardapioDigital.Domain/Models/Restaurante.cs
+++ src/CardapioDigital.Domain/Models/Restaurante.cs
@@ -2,12 +2,14 @@
 
 public sealed class Restaurante
 {
-    public Restaurante(string imagemRestaturanteUrl)
+    public Restaurante(string nomeRestaurante, string imagemRestaturanteUrl)
     {
+        NomeRestaurante = nomeRestaurante;
         ImagemRestaturanteUrl = imagemRestaturanteUrl;
     }
 
     public int IdRestaurante { get; set; }
+    public string NomeRestaurante { get; set; }
     public string ImagemRestaturanteUrl { get; set; }
 
     public IEnumerable<Produto> Produtos { get; set; }
