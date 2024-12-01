0ee0f745c9f4a9dcb0799d909f48041301238fc0
Wesley Farias <wesleyfarias.dev@gmail.com>
2024-12-01 03:30:11 -0300

    adicionado camada de application

---
+++ src/CardapioDigital.Application/Dto/Response/RestauranteResponseDto.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.Json.Serialization;
+
+namespace CardapioDigital.Application.Dto.Response;
+
+public class RestauranteResponseDto
+{
+    [JsonPropertyName("idRestaurante")]
+    public int IdRestaurante { get; init; }
+
+    [JsonPropertyName("nomeRestaurante")]
+    public string NomeRestaurante { get; init; }
+
+    [JsonPropertyName("imagemRestauranteUrl")]
+    public string ImagemRestaturanteUrl { get; init; }
+
+}
---
+++ src/CardapioDigital.Application/Services/IServices/IGenericoServices.cs
@@ -0,0 +1,10 @@
+﻿namespace CardapioDigital.Application.Services.IServices;
+
+public interface IGenericoServices<T>
+{
+    Task<IEnumerable<T>> GetAll();
+    Task<T> GetById(int id);
+    Task<T> Update(T entity);
+    Task<T> Create(T entity);
+    Task<bool> DeleteById(int id);
+}
