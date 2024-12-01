4cdcb8a44a9b0fdf74fdaf6628b7cfe1282d5cf8
Wesley Farias <wesleyfarias.dev@gmail.com>
2024-12-01 03:06:12 -0300

    Adiciona base do projeto domain

---
+++ src/CardapioDigital.Domain/Interface/IGenericoRepositorio.cs
@@ -0,0 +1,10 @@
+﻿namespace CardapioDigital.Domain.Interface;
+
+public interface IGenericoRepositorio<T>
+{
+    Task<IEnumerable<T>> GetAll();
+    Task<T> GetById(int id);
+    Task<T> Update(T entity);
+    Task<T> Create(T entity);
+    Task<bool> DeleteById(int id);
+}
---
+++ src/CardapioDigital.Domain/Models/Restaurante.cs
@@ -0,0 +1,14 @@
+﻿namespace CardapioDigital.Domain.Models;
+
+public sealed class Restaurante
+{
+    public Restaurante(string imagemRestaturanteUrl)
+    {
+        ImagemRestaturanteUrl = imagemRestaturanteUrl;
+    }
+
+    public int IdRestaurante { get; set; }
+    public string ImagemRestaturanteUrl { get; set; }
+
+    public IEnumerable<Produto> Produtos { get; set; }
+}
