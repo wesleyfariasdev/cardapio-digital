d4b3074576dc8ba519348edbde4a43e879d7c249
Wesley Farias <wesleyfarias.dev@gmail.com>
2024-12-01 19:38:05 -0300

    Implementa mapConfig

---
+++ src/CardapioDigital.Application/Mappings/MapConfig.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using CardapioDigital.Application.Dto.Request;
+using CardapioDigital.Application.Dto.Response;
+using CardapioDigital.Domain.Models;
+
+namespace CardapioDigital.Application.Mappings;
+
+public class MapConfig : Profile
+{
+    public MapConfig()
+    {
+        CreateMap<Produto, ProdutoRequestDto>().ReverseMap();
+        CreateMap<Produto, ProdutoResponseDto>().ReverseMap();
+
+        CreateMap<Categoria, CategoriaResponseDto>().ReverseMap();
+        CreateMap<Categoria, CategoriaResponseDto>().ReverseMap();
+
+        CreateMap<Restaurante, RestauranteResponseDto>().ReverseMap();
+        CreateMap<Restaurante, RestauranteResponseDto>().ReverseMap();
+    }
+}
