3c76f1f91e608ff3b117b946bd7c9d394eaa3461
Wesley Farias <wesleyfarias.dev@gmail.com>
2024-12-01 19:38:38 -0300

    Adiciona dependencia dos services ao projeto web

--- CardapioDigital.Web/Program.cs
+++ CardapioDigital.Web/Program.cs
@@ -1,3 +1,4 @@
+using CardapioDigital.Infra.IoC;
 using CardapioDigital.Web;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
@@ -7,5 +8,6 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddInfrastructure();
 
 await builder.Build().RunAsync();
