0233e955b6a9ba68b0a7f23d4cd9694ebedf4671
Wesley Farias <wesleyfarias.dev@gmail.com>
2024-12-02 20:07:31 -0300

    injeta dependencia

--- CardapioDigital.Web/Program.cs
+++ CardapioDigital.Web/Program.cs
@@ -9,5 +9,6 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddInfrastructure();
+builder.Services.AddInfraestructureDb(builder.Configuration);
 
 await builder.Build().RunAsync();
