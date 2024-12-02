00e2598d215e76b24ce5d4706e37df340e11f5c0
Wesley Farias <wesleyfarias.dev@gmail.com>
2024-12-02 20:07:19 -0300

    adiciona string de conexão com o banco

--- src/CardapioDigital.Infra.IoC/Db.cs
+++ src/CardapioDigital.Infra.IoC/Db.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using CardapioDigital.Infra.Data.CardapioDb;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +10,17 @@ using System.Threading.Tasks;
 
 namespace CardapioDigital.Infra.IoC
 {
-    internal class Db
+    internal static class Db
     {
+        public static IServiceCollection AddInfraestructureDb(this IServiceCollection services, IConfiguration config)
+        {
+            services.AddDbContext<CardapioDbContext>(options => options
+                    .UseSqlServer(config.GetConnectionString(@"Server=(localdb)\\mssqllocaldb;Database=LOLProfile;Trusted_Connection=True;
+                       MultipleActiveResultSets=true"), x => x
+                   .MigrationsAssembly(typeof(CardapioDbContext).Assembly.FullName)));
+
+
+            return services;
+        }
     }
 }
