d7b2f27482ad7923ab827a90f1d541c94735b5cc
Wesley Farias <wesleyfarias.dev@gmail.com>
2024-12-02 22:48:01 -0300

    adicionado projeto IoC

---
+++ src/CardapioDigital.Infra.IoC/Db.cs
@@ -0,0 +1,20 @@
+﻿using CardapioDigital.Infra.Data.CardapioDb;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CardapioDigital.Infra.IoC
+{
+    internal static class Db
+    {
+        public static IServiceCollection AddInfraestructureDb(this IServiceCollection services, IConfiguration config)
+        {
+            services.AddDbContext<CardapioDbContext>(options => options
+                    .UseSqlServer(config.GetConnectionString("Dev"), x => x
+                   .MigrationsAssembly(typeof(CardapioDbContext).Assembly.FullName)));
+
+
+            return services;
+        }
+    }
+}
---
+++ src/CardapioDigital.Infra.IoC/IoC.cs
@@ -0,0 +1,29 @@
+﻿using CardapioDigital.Application.Dto.Request;
+using CardapioDigital.Application.Dto.Response;
+using CardapioDigital.Application.Mappings;
+using CardapioDigital.Application.Services;
+using CardapioDigital.Application.Services.IServices;
+using CardapioDigital.Domain.Interface;
+using CardapioDigital.Domain.Models;
+using CardapioDigital.Infra.Data.Repository;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CardapioDigital.Infra.IoC;
+
+internal static class IoC
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+    {
+        services.AddScoped<IGenericoServices<CategoriaRequestDto, CategoriaResponseDto>, CategoriaService>();
+        services.AddScoped<IGenericoServices<ProdutoRequestDto, ProdutoResponseDto>, ProdutoService>();
+        services.AddScoped<IGenericoServices<RestauranteRequestDto, RestauranteResponseDto>, RestauranteService>();
+
+        services.AddScoped<IGenericoRepositorio<Categoria>, CategoriaRepository>();
+        services.AddScoped<IGenericoRepositorio<Produto>, ProdutoRepository>();
+        services.AddScoped<IGenericoRepositorio<Restaurante>, RestauranteRepository>();
+
+        services.AddAutoMapper(typeof(MapConfig));
+
+        return services;
+    }
+}
