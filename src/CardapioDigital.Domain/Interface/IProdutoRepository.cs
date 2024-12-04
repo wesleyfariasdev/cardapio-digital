3076fae8588548c1d8c69a5ae52a5395c49cde0e
Wesley Farias <wesleyfarias.dev@gmail.com>
2024-12-04 01:06:54 -0300

    interface para produto

---
+++ src/CardapioDigital.Domain/Interface/IProdutoRepository.cs
@@ -0,0 +1,8 @@
+﻿using CardapioDigital.Domain.Models;
+
+namespace CardapioDigital.Domain.Interface;
+
+internal interface IProdutoRepository
+{
+    Task<IEnumerable<Produto>> GetProdutosById(int idRestaurante);
+}
